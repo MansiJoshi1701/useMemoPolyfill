bcfe4a7239eb3f1c50fb9a2046236e487288ffcf
MansiJoshi1701 <mansijoshi.mj1798@gmail.com>
2025-08-25 16:11:56 -0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useMemo, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [counter1, setCounter1] = useState(0);
+  const [counter2, setCounter2] = useState(100);
+
+  const squaredValue = () => {
+    console.log("Expensive calculation");
+    return counter1 * counter1;
+  }
+
+  const memoizedValue = useMemo(squaredValue , [counter1]);
+
+  return (
+    <div className='App'>
+      <h2>Counter: {counter1}</h2>
+      <h2>Squared Counter: {memoizedValue}</h2>
+      <button onClick={() => setCounter1(counter1+1)}>Increment</button>
+      <h2>Counter 2: {counter2}</h2>
+      <button onClick={() => setCounter2(counter2-1)}>Decrement</button>
+    </div>
+  )
+}
+
+export default App
